b990b1b074b6fb6871f3fd4a7e2a6d26297bd77a
leo-demetrio <leocdemetrio@gmail.com>
2022-01-06 12:14:54 -0300

    service layer, componente resitration

--- src/main/java/com/leodev/api/resources/UserResource.java
+++ src/main/java/com/leodev/api/resources/UserResource.java
@@ -1,18 +1,29 @@
 package com.leodev.api.resources;
 
 import com.leodev.api.entities.User;
+import com.leodev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Maria","maria@gmail.ocm","9999999","123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
