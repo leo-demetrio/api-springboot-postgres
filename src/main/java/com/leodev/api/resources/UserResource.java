dd17672c7b407a27de979fb5a522e496ae092ab8
leo-demetrio <leocdemetrio@gmail.com>
2022-04-12 12:22:25 -0300

    Insert User

--- src/main/java/com/leodev/api/resources/UserResource.java
+++ src/main/java/com/leodev/api/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.leodev.api.entities.User;
 import com.leodev.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +24,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        User obj = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
